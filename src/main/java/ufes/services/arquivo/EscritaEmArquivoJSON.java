dd88785446653f7b5ce348a0dbb84985e5cfd03c
mtknack <mtknack555@gmail.com>
2023-06-29 13:53:50 -0300

    Acetando erros

--- src/main/java/ufes/services/arquivo/EscritaEmArquivoJSON.java
+++ src/main/java/ufes/services/arquivo/EscritaEmArquivoJSON.java
@@ -1,55 +1,67 @@
 package ufes.services.arquivo;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import javax.swing.JOptionPane;
 
-public class EscritaEmArquivoJSON implements IArquivo{
+public class EscritaEmArquivoJSON implements IArquivo {
     private static EscritaEmArquivoJSON instancia;
-    String currentDirectory = System.getProperty("user.dir");
-    private final String filePath =  currentDirectory + "/src/main/java/ufes/services/arquivo/LogJSON.json";
-    
+    private final String filePath = "src/main/java/ufes/services/arquivo/LogJSON.json";
+
     private FileWriter fw;
     private File file;
-    
-    private EscritaEmArquivoJSON(){
-        try{
+
+    private EscritaEmArquivoJSON() {
+        try {
             this.file = new File(filePath);
             if (!file.exists()) {
                 file.createNewFile();
             }
-        }
-        catch(IOException exIO){
-            JOptionPane.showMessageDialog(null, "Nao foi possivel ler o arquivo");
+        } catch (IOException exIO) {
+            JOptionPane.showMessageDialog(null, "Não foi possível ler o arquivo");
         }
     }
-    
-    public static EscritaEmArquivoJSON getInstancia(){
-        if(instancia == null){
+
+    public static EscritaEmArquivoJSON getInstancia() {
+        if (instancia == null) {
             EscritaEmArquivoJSON.instancia = new EscritaEmArquivoJSON();
         }
         return EscritaEmArquivoJSON.instancia;
     }
-    
-    @Override
-    public void escreverArquivo(String log){
 
+    @Override
+    public void escreverArquivo(String log) {
         try {
-            this.fw = new FileWriter(file, true);
-            
-            // Verifica se o arquivo está vazio
+            FileWriter fw = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+
             if (file.length() != 0) {
-                fw.write(",\n");
+                // Apaga a última linha que contém o caractere ']'
+                RandomAccessFile raf = new RandomAccessFile(file, "rw");
+                long length = raf.length();
+                raf.setLength(length - 2);
+                raf.close();
+                
+                bw.write(",\n");
+            } else {
+                bw.write("[\n"); 
             }
-            
-            fw.write(log);
-            fw.close();
+
+            bw.write(log);
+
+            bw.close();
+
+            // Adiciona o fechamento do array depois de escrever o objeto
+            FileWriter fw2 = new FileWriter(file, true);
+            BufferedWriter bw2 = new BufferedWriter(fw2);
+
+            bw2.write("\n]");
+            bw2.close();
         } catch (IOException ioEx) {
             JOptionPane.showMessageDialog(null, ioEx);
         }
-
-
-        
     }
 }
